be94b0cb64aa6ab70a74b25613f1f2eb6cf6f814
Jordan Davidson <42349125+jordandavidson1990@users.noreply.github.com>
2020-04-23 15:23:30 +0100

    Jd/bar change (#3)
    * enemy health bar changes depending on how much health is left
    
    * x

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ProgressLine from "./ProgressLine";
 
-export default function ProgressBar({ enemy }) {
-  const percentageLevel = (enemy.health / enemy.originalHealth) * 100;
+export default function ProgressBar({ character }) {
+  const percentageLevel = (character.health / character.originalHealth) * 100;
+
   const percentageColour = () => {
     if (percentageLevel >= 80) {
       return "green";
