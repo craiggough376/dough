f325d38a0ec739b3672aed0206fb88f813dc5b85
Jordan Davidson <jordan.davidson@arnoldclark.com>
2020-04-29 16:11:27 +0100

    used imports for components

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Enemy } from "./Enemy";
+export { default as PlayerInput } from "./PlayerInput";
+export { default as PlayerStats } from "./PlayerStats";
+export { default as StatTable } from "./StatTable";
+export { default as PotionGift } from "./Gift/PotionGift";
+export { default as ProgressBar } from "./ProgressBar/index";
